304107b2cff8cc9c98b4bb06ecdc787ac0e02a25
Biola <52200708+decorouz@users.noreply.github.com>
2022-07-20 18:25:25 +0100

    put wrapper around function

---
+++ Metaprogramming/wrapper_func.py
@@ -0,0 +1,32 @@
+# PROBLEM: You want to put a wrapper layer around a function that adds extra processing
+
+import time
+from functools import wraps
+
+
+def timethis(func):
+    """
+    Decorator that reports the execution time.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end - start)
+        return result
+
+    return wrapper
+
+
+# Example
+
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+
+countdown = timethis(countdown)
+countdown(1000)
